756e4c236875d0ece28615d0aa9a53da1c75b00f
BatmanBoxer <darwinmage98422@gmail.com>
2024-07-29 18:24:42 +0545

    final final

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import kunmanga from "./routes/KunManga/KunManga.js";
-import manganato from "./routes/Manganato/Manganatoo.js"
+import manganato from "./routes/Manganato/Manganatoo.js";
+import welcome from "./routes/Welcome/Welcome.js";
 
 const port = process.env.PORT || 1000;
 const app = express();
 
 app.use("/kunmanga", kunmanga);
-app.use("/manganato",manganato);
+app.use("/manganato", manganato);
+app.use("/", welcome);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
