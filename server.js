f420469cbf67d069948eedf132519555d8f75962
Batman <darwinkoirala123@gmail.com>
2024-08-11 21:28:11 +0545

    fuck vercel

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import kunmanga from "./routes/KunManga/KunManga.js";
 import manganato from "./routes/Manganato/Manganatoo.js";
 import Welcome from "./routes/Welcome.js";
-const port = process.env.PORT || 8081;
+const port =8081;
 const app = express();
 
 app.use("/kunmanga", kunmanga);
@@ -11,4 +11,4 @@ app.use("/", Welcome);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-});
+})
