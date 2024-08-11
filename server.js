08d176573beb273ef253c3889f2c615f34953ec3
Batman <darwinkoirala123@gmail.com>
2024-08-11 21:12:00 +0545

    final

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 import express from "express";
 import kunmanga from "./routes/KunManga/KunManga.js";
 import manganato from "./routes/Manganato/Manganatoo.js";
-
-const port = process.env.PORT || 1000;
+import Welcome from "./routes/Welcome.js";
+const port = process.env.PORT || 8081;
 const app = express();
 
 app.use("/kunmanga", kunmanga);
 app.use("/manganato", manganato);
+app.use("/", Welcome);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
